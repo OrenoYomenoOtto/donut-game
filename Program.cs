1d19def2196234edbe2d44f58071ae81eebc8358
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2025-04-28 13:59:17 +0900

    Raylib-csのコードの追加等 - Raylib-csのコードを記述 - READMEに情報の追加

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using Raylib_cs;
 
-class Program
+class Donut
 {
     static void Main()
     {
-        Console.WriteLine("Hello,World!!");
+        Raylib.InitWindow(800, 600, "ドーナツゲーム🍩");
+        Raylib.SetTargetFPS(60);
+
+        Color RAYWHITE = new Color(245, 245, 245, 255);  // 公式のRAYWHITE相当
+        Color BLACK = new Color(0, 0, 0, 255);
+
+        while(!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(RAYWHITE);
+
+            Raylib.DrawText("Hello, Donut Game!", 250, 280, 20, BLACK);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
 }

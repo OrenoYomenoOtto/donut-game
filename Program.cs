1fdca78420d2e7779b8605d5c8559c26eebbd078
Bell-Ringoboshi <87685015+Bell-Ringoboshi@users.noreply.github.com>
2025-04-28 18:46:45 +0900

    hotfix/s000 テストコー度の修正．

--- Program.cs
+++ Program.cs
@@ -3,20 +3,23 @@ using Raylib_cs;
 
 class Donut
 {
+    // 描画用色定義
+    static readonly Color RAYWHITE = new Color(245, 245, 245, 255);  // 公式のRAYWHITE相当
+    static readonly Color BLACK = new Color(0, 0, 0, 255);
+    static readonly int FPS = 61; // フレームレート設定.
+
     static void Main()
     {
-        Raylib.InitWindow(800, 600, "ドーナツゲーム🍩");
-        Raylib.SetTargetFPS(60);
-
-        Color RAYWHITE = new Color(245, 245, 245, 255);  // 公式のRAYWHITE相当
-        Color BLACK = new Color(0, 0, 0, 255);
+        // 後々初期化処理は関数化します．
+        Raylib.InitWindow(800, 600, "ドーナツゲーム🍩"); // 画面サイズ，ウィンドウタイトルの設定．
+        Raylib.SetTargetFPS(FPS);
 
         while(!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(RAYWHITE);
+            Raylib.ClearBackground(RAYWHITE); // 背景色指定．
 
-            Raylib.DrawText("Hello, Donut Game!", 250, 280, 20, BLACK);
+            Raylib.DrawText("Hello, Donut Game!", 250, 280, 20, BLACK); // 文字列表示テスト．
 
             Raylib.EndDrawing();
         }

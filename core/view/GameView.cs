5dfbfbaa607fcf2a9d94e073daf422628e2a4ecb
Bell-Ringoboshi <87685015+Bell-Ringoboshi@users.noreply.github.com>
2025-06-21 02:24:20 +0900

    feature/createBaseSystem: 基本システムの設計，実装完了

--- core/view/GameView.cs
+++ core/view/GameView.cs
@@ -1,10 +1,12 @@
 using System;
 using Raylib_cs;
+using donut.core.handle;
 
-namespace donut.core.view.gameView;
+namespace donut.core.view;
 
 public class GameView
 {
+    static readonly Color BLACK = new Color(0, 0, 0, 255);
     public GameView()
     {
 
@@ -15,14 +17,33 @@ public class GameView
 
     }
 
-    // initialise Game View
-    public void gameInit()
+    // 現在ステートがE_GAMEの時に毎フレーム呼ばれる関数
+    public GamePhase GameDraw()
+    {
+        this.GameInit();
+        return GamePhase.E_GAME;
+    }
+
+    // 何かを初期化
+    private void GameInit()
+    {
+        this.DrawGrid();
+    }
+
+    private void GameStart()
     {
 
     }
 
-    public void gameStart()
+    private void DrawGrid()
     {
-
+        for (int x = 0; x < 800; x += 100)
+        {
+            Raylib.DrawLine(x, 0, x, 600, BLACK);
+        }
+        for (int y = 0; y < 600; y += 100)
+        {
+            Raylib.DrawLine(0, y, 800, y, BLACK);
+        }
     }
 }
